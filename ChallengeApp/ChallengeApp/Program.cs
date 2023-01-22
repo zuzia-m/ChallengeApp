7ad76d0fef272574b9a872e2955405624568d3cb
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2023-01-22 08:43:31 +0100

    day-4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Zuzia";
+var isFemale = true;
+var age = 25;
+
+if (isFemale && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (!isFemale && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
