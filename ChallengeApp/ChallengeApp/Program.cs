7b9846d1f14e0c5f8b713d10545aca108ce6420c
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2023-01-22 08:56:17 +0100

    day-5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,18 @@
-﻿var name = "Zuzia";
-var isFemale = true;
-var age = 25;
+﻿int number = 1458199756;
+string numberInString = number.ToString();
+char[] digitsInNumber = numberInString.ToArray();
+List<char> digits = new() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-if (isFemale && age < 30)
+foreach (var digit in digits)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (!isFemale && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    int count = 0;
+    foreach (var digitInNumber in digitsInNumber)
+    {
+        if (digitInNumber == digit)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"{digit} => {count}");
 }
