2161a3e571a40c40a1d6dea3cd97023d56832608
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2023-01-26 18:12:52 +0100

    day-8

---
+++ ChallengeApp/ChallengeAppTests/TypeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TwoStringsAreEqual()
+        {
+            var string1 = "tralalala";
+            var string2 = "tralalala";
+
+            Assert.AreEqual(string1, string2);
+        }
+
+        [Test]
+        public void TwoCharsAreEqual()
+        {
+            var char1 = 'o';
+            var char2 = 'o';
+
+            Assert.AreEqual(char1, char2);
+        }
+
+        [Test]
+        public void TwoSameIntsAreEqual()
+        {
+            var number1 = 1;
+            var number2 = 1;
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void GetEmployeeReturnsDifferentObjects()
+        {
+            var emp1 = GetEmployee("Lionel", "Messi", 35);
+            var emp2 = GetEmployee("Luis", "Suarez", 35);
+
+            Assert.AreNotEqual(emp1, emp2);
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+}
