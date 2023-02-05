e51ffb7b8c5598586ad9160455e831ee395ab438
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2023-02-05 18:05:29 +0100

    day-12 - refactor

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -16,7 +16,7 @@ while (true)
 }
 
 var statistics = employee.GetStatistics();
-Console.WriteLine("Employee's :");
+Console.WriteLine("Statistics:");
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
